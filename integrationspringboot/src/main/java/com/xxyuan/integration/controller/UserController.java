426a91543bf8516aa7a3f065967381b66c7d96a5
xiaoyuan916 <402973804@qq.com>
2019-01-04 12:10:24 +0800

    添加跨域访问问题

--- integrationspringboot/src/main/java/com/xxyuan/integration/controller/UserController.java
+++ integrationspringboot/src/main/java/com/xxyuan/integration/controller/UserController.java
@@ -6,6 +6,7 @@ import com.xxyuan.integration.model.User;
 import com.xxyuan.integration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +28,7 @@ public class UserController {
     }
 
     @ResponseBody
+    @CrossOrigin
     @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
 
