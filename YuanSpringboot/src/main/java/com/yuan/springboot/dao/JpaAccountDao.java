f30230c256f016db0f57579a07b89d0c09d46c02
xiaoyuan916 <402973804@qq.com>
2019-01-04 15:40:32 +0800

    移除不用的项目

--- YuanSpringboot/src/main/java/com/yuan/springboot/dao/JpaAccountDao.java
+++
@@ -1,7 +0,0 @@
-//package com.yuan.springboot.dao;
-//
-//import com.yuan.springboot.domain.JpaAccount;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface JpaAccountDao extends JpaRepository<JpaAccount, Integer> {
-//}
