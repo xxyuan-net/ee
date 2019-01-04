396928725755909ef926e652c852a70bceb8fc45
xiaoyuan916 <402973804@qq.com>
2019-01-04 10:28:28 +0800

    初次搭建springboot 整合mybatis的项目

---
+++ integrationspringboot/src/main/java/com/xxyuan/integration/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.xxyuan.integration.mapper;
+
+import com.xxyuan.integration.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    List<User> selectAllUser();
+}
