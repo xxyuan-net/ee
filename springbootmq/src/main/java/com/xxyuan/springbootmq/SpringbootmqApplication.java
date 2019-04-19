11def95e1096e36353ad8413fb9ab9ed721c6b49
xiaoyuan916 <402973804@qq.com>
2019-04-19 11:48:09 +0800

    创建一个springboot使用mq的样例

---
+++ springbootmq/src/main/java/com/xxyuan/springbootmq/SpringbootmqApplication.java
@@ -0,0 +1,13 @@
+package com.xxyuan.springbootmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmqApplication.class, args);
+    }
+
+}
---
+++ springbootmq/src/test/java/com/xxyuan/springbootmq/SpringbootmqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xxyuan.springbootmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
